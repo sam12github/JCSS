dc4d8d4c3a15fd8a28e805a69806ea68abd95105
Sam <sinfx12@gmail.com>
2013-10-10 21:23:34 -0400

    Update jcss.js

--- jcss.js
+++ jcss.js
@@ -1,1 +1,8 @@
-################## JCSS #######################
+//################## JCSS #######################
+
+jQuery.expr[':'].css = function(elem,arge1 ) {
+$("p" ) .append ( arge1);
+        return jQuery(elem).css('display') === 'inline';
+
+};
+$("ul") .append (   $("li:css").length  ); 
