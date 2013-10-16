bc4de5db2c2706a5b7d69a24f00812fc34daf8b4
Sam <sinfx12@gmail.com>
2013-10-15 22:29:21 -0400

    Update jcss.js
    first release

--- jcss.js
+++ jcss.js
@@ -1,8 +1,10 @@
 //################## JCSS #######################
 
-jQuery.expr[':'].css = function(elem,arge1 ) {
-$("p" ) .append ( arge1);
-        return jQuery(elem).css('display') === 'inline';
+ jQuery(document).ready(function () {
+     jQuery.expr[':'].css = function (elem, arge1, arge2) {
+         // $("#new").append(elem);
+         return jQuery(elem).css(arge2[3].split(",")[0]) === arge2[3].split(",")[1];
 
-};
-$("ul") .append (   $("li:css").length  ); 
+     };
+    
+ });
